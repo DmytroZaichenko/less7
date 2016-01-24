335cf23816843c156e2f1ad1a3b80e269dd85e6d
Dmytro Zaichenko <dim2002@ukr.net>
2016-01-24 21:23:12 +0200

    with list birds

--- src/generics/domain/Box.java
+++ src/generics/domain/Box.java
@@ -1,10 +1,15 @@
 package generics.domain;
 
-public class Box<T> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Bird> {
 
     private T item;
+    private List<T> lists;
 
     public Box() {
+        lists = new ArrayList<>();
     }
 
     public T getItem() {
@@ -14,4 +19,17 @@ public class Box<T> {
     public void setItem(T item) {
         this.item = item;
     }
+
+    public void add(T t){
+        lists.add(t);
+    }
+
+    public void del(T t){
+        lists.remove(t);
+    }
+
+    public List<T> getLists() {
+        return lists;
+    }
+
 }
--- src/generics/domain/Demo.java
+++ src/generics/domain/Demo.java
@@ -1,11 +1,24 @@
 package generics.domain;
 
+import java.util.List;
+
 public class Demo {
 
     public static void main(String[] args) {
-        Box<Integer> box = new Box<>();
-        box.setItem(34);
-        System.out.println(box.getItem());
+
+
+        Box<Bird> products = new Box<>();
+
+        Box<Bird> birds = new Box<>();
+        birds.add(new Duck());
+        birds.add(new Eagle());
+
+        for (Bird b: birds.getLists()) {
+            System.out.println(b.toString());
+        }
+
+
+
     }
 
 }
